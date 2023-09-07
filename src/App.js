7c0cf17d279c3d5356c526fce015bc82c35bccb5
juliusalidon <juliusalidon@gmail.com>
2023-09-07 11:49:13 +0800

    first react repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import NavigationBar from "./components/NavigationBar";
+import Banner from "./components/Banner";
+import Image1 from "./images/2nd.jpg";
+import Image2 from "./images/3rd.jpg";
+import Image3 from "./images/4th.jpg";
+import Image4 from "./images/5th.jpg";
+import Image5 from "./images/6th.jpg";
+import Image6 from "./images/7th.jpg";
+import GridItems from "./components/GridItems";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+       <NavigationBar></NavigationBar>
+       <Banner></Banner>
+      <div className="bg-dark p-5">
+        <div className="container">
+          <h1 className="text-warning">Top 5 Animes of the Week:</h1>
+          <div className="row">
+           <GridItems name="Dragon Ball Z" imageFile={Image1} desc="Lorem Ipsum"></GridItems>
+           <GridItems name="One Piece" imageFile={Image2} desc="Lorem Ipsum"></GridItems>
+           <GridItems name="Naruto" imageFile={Image3} desc="Lorem Ipsum"></GridItems>
+           <GridItems name="Bleach" imageFile={Image4} desc="Lorem Ipsum"></GridItems>
+           <GridItems name="Yuyu Hakusho" imageFile={Image5} desc="Lorem Ipsum"></GridItems>
+           <GridItems name="Slam Dunk" imageFile={Image6} desc="Lorem Ipsum"></GridItems>
+        </div>
+        </div>
+      </div>
+      <Footer></Footer>
+    </>
   );
 }
 
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import BannerImage from "../images/1st.jpg";
+
+const Banner = () => {
+    return ( 
+        <>
+           <img src={BannerImage} alt="Banner Image" className="w-100"></img>
+        </>
+     );
+}
+ 
+export default Banner;
